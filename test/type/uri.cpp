6d22515266cc3a30daf0f6f2d80436369297a26e
Parsa Ghadimi <qti3eqti3e@gmail.com>
2019-12-17 14:30:47 +0330

    Fix uri test

--- test/type/uri.cpp
+++ test/type/uri.cpp
@@ -11,30 +11,20 @@ TEST("uri") {
   Type::Uri uri, uri2;
   std::vector<std::string> units;
 
-  std::cout << "AAA" << std::endl;
-
   CHECK(uri.isEmpty() == true);
-
-  std::cout << "BBB" << std::endl;
 
   uri = Type::Uri("A", "B");
   CHECK(uri.isEmpty() == false);
-
-  std::cout << "CCCC" << std::endl;
 
   units = uri.getUnits();
   CHECK(units.size() == 2);
   CHECK(units[0] == "A");
   CHECK(units[1] == "B");
 
-  std::cout << "DDDD" << std::endl;
-
   uri2 = uri.popFirst();
   units = uri2.getUnits();
   CHECK(units.size() == 1);
-  CHECK(units[1] == "B");
-
-  std::cout << "EEEEE" << std::endl;
+  CHECK(units[0] == "B");
 
   units = uri.getUnits();
   CHECK(units.size() == 2);
