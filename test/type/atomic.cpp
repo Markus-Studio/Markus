64f7f9ec92d62318ac0570f780365d7f82acd395
Parsa Ghadimi <qti3eqti3e@gmail.com>
2019-12-17 13:49:33 +0330

    Add more tests

--- test/type/atomic.cpp
+++ test/type/atomic.cpp
@@ -1,5 +1,52 @@
-#include "../test.hpp"
+#include "type/atomic.hpp"
 
-TEST("Hello") {
-  CHECK_TRUE(true);
+#include "../test.hpp"
+#include "type/union.hpp"
+#include "type/uri.hpp"
+
+TEST("Atomic") {
+  Type::Atomic atomicString("string");
+  Type::Atomic atomicInt("int");
+  Type::Atomic atomicFloat("float");
+
+  CHECK(atomicString.is(&atomicString) == true);
+  CHECK(atomicString.is(&atomicInt) == false);
+  CHECK(atomicInt.getName() == "int");
+
+  Type::Union union1;
+  Type::Union union2;
+  Type::Union union3;
+
+  CHECK(union1.isEmpty() == true);
+  CHECK(union2.isEmpty() == true);
+  CHECK(union3.isEmpty() == true);
+
+  CHECK(union1.has(&atomicString) == false);
+  CHECK(atomicString.is(&union1) == false);
+
+  union1.add(&atomicString);
+  CHECK(union1.isEmpty() == false);
+
+  CHECK(union1.has(&atomicString) == true);
+  CHECK(atomicString.is(&union1) == true);
+
+  union2 = union1.clone();
+  CHECK(union2.has(&atomicString) == true);
+  CHECK(atomicString.is(&union2) == true);
+  CHECK(union2.isEmpty() == false);
+
+  union2.add(&atomicFloat);
+  CHECK(union2.has(&atomicFloat) == true);
+  CHECK(atomicFloat.is(&union2) == true);
+
+  CHECK(union1.has(&atomicFloat) == false);
+  CHECK(atomicFloat.is(&union1) == false);
+
+  union3.add(&union1);
+  union3.add(&union2);
+  CHECK(union3.isEmpty() == false);
+
+  CHECK(union3.has(&atomicString) == true);
+  CHECK(union3.has(&atomicFloat) == true);
+  CHECK(union3.has(&atomicInt) == false);
 }
