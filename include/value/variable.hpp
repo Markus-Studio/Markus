ffebe15a1726c21be830aa72360138d4b028c92e
Parsa Ghadimi <qti3eqti3e@gmail.com>
2019-12-20 23:16:34 +0330

    Value::Variable - Add the header file

---
+++ include/value/variable.hpp
@@ -0,0 +1,68 @@
+#ifndef __MARKUS_VALUE__VARIABLE__
+#define __MARKUS_VALUE__VARIABLE__
+
+#include "type/container.hpp"
+#include "type/uri.hpp"
+
+namespace Value {
+/**
+ * A reference to a variable, either internal or parameter.
+ */
+class Variable {
+ private:
+  /**
+   * Type of the base.
+   */
+  Type::Container* variableType;
+
+  /**
+   * The parameter id for this variable.
+   */
+  int variableId;
+
+  /**
+   * If there is a member selection on this variable.
+   */
+  bool hasVariableMember;
+
+  /**
+   * The selected member from the base if any.
+   */
+  Type::Uri* variableMember;
+
+ public:
+  /**
+   * Constructs a new reference to a variable.
+   */
+  Variable(int id, Type::Container* type);
+
+  /**
+   * Constructs a new reference to a variable with a member selection.
+   */
+  Variable(int id, Type::Container* type, Type::Uri* member);
+
+  /**
+   * Returns true if there is a member selected on this variable.
+   */
+  bool hasMember();
+
+  /**
+   * Returns the type of the base, without considering selected member.
+   * $point.x -> Point2D
+   */
+  Type::Container* getBaseType();
+
+  /**
+   * Returns the type of this entity, considers the selected member.
+   * $point.x -> float
+   */
+  Type::Container* getType();
+
+  /**
+   * Returns uri of the selected member.
+   */
+  Type::Uri* getMember();
+};
+}  // namespace Value
+
+#endif
