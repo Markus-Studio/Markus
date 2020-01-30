6ce50d12befcf9d5fca8d23d176ee39acd865799
Parsa Ghadimi <qti3eqti3e@gmail.com>
2020-01-30 13:55:46 +0330

    Add a test in test_type_declaration_error_tolerance

--- rust/tests/ast.rs
+++ rust/tests/ast.rs
@@ -264,4 +264,42 @@ fn test_type_declaration_error_tolerance() {
             })]
         );
     }
+
+    {
+        let source = "type A: X, Y { x  number  p?  string";
+        let mut program = Program::new(Source::new(String::from("foo.x"), source));
+        program.parse();
+        assert_eq!(program.get_span_data_as_utf8(Span::new(5, 1)), "A");
+        assert_eq!(program.get_span_data_as_utf8(Span::new(8, 1)), "X");
+        assert_eq!(program.get_span_data_as_utf8(Span::new(11, 1)), "Y");
+        assert_eq!(program.get_span_data_as_utf8(Span::new(15, 1)), "x");
+        assert_eq!(program.get_span_data_as_utf8(Span::new(18, 6)), "number");
+        assert_eq!(program.get_span_data_as_utf8(Span::new(26, 1)), "p");
+        assert_eq!(program.get_span_data_as_utf8(Span::new(30, 6)), "string");
+        assert_eq!(
+            program.declarations,
+            vec![ast::Declaration::Type(ast::TypeDeclarationNode {
+                location: Span::new(0, 36),
+                name: Some(ast::IdentifierNode::new(5, 1)),
+                bases: vec![
+                    ast::IdentifierNode::new(8, 1),
+                    ast::IdentifierNode::new(11, 1)
+                ],
+                fields: vec![
+                    ast::TypeFieldNode {
+                        location: Span::new(15, 9),
+                        nullable: false,
+                        name: Some(ast::IdentifierNode::new(15, 1)),
+                        type_name: Some(ast::IdentifierNode::new(18, 6)),
+                    },
+                    ast::TypeFieldNode {
+                        location: Span::new(26, 10),
+                        nullable: true,
+                        name: Some(ast::IdentifierNode::new(26, 1)),
+                        type_name: Some(ast::IdentifierNode::new(30, 6)),
+                    }
+                ]
+            })]
+        );
+    }
 }
