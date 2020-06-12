9dbc61c74899354dd3d2ae50804979b62ed6a904
Parsa Ghadimi <qti3eqti3e@gmail.com>
2020-06-12 21:51:33 +0430

    Change type field grammar

--- frontend/tree-sitter-markus/grammar.js
+++ frontend/tree-sitter-markus/grammar.js
@@ -1,11 +1,11 @@
 module.exports = grammar({
   name: "markus",
-  word: ($) => $.identifier,
-  extras: ($) => [$.comment, /[\s\uFEFF\u2060\u200B\u00A0]/],
-  supertypes: ($) => [$._statement, $._declaration, $._value],
+  word: $ => $.identifier,
+  extras: $ => [$.comment, /[\s\uFEFF\u2060\u200B\u00A0]/],
+  supertypes: $ => [$._statement, $._declaration, $._value],
   rules: {
-    source_file: ($) => repeat($._declaration),
-    _declaration: ($) =>
+    source_file: $ => repeat($._declaration),
+    _declaration: $ =>
       choice(
         $.type_declaration,
         $.permission_declaration,
@@ -13,19 +13,20 @@ module.exports = grammar({
         $.action_declaration
       ),
     // -------- TYPE DECLARATION.
-    type_declaration: ($) =>
+    type_declaration: $ =>
       seq(
         "type",
         field("name", $.identifier),
         optional(seq(":", $._bases_list)),
         "{",
-        field("body", optional($.type_fields)),
+        optional($._type_fields),
         "}"
       ),
-    _bases_list: ($) =>
+    _bases_list: $ =>
       seq(commaSep1(field("base", $.identifier)), optional(",")),
-    type_fields: ($) => seq(commaSep1($.type_field), optional(",")),
-    type_field: ($) =>
+    _type_fields: $ =>
+      seq(commaSep1(field("field", $.type_field)), optional(",")),
+    type_field: $ =>
       seq(
         field("name", $.identifier),
         field("optional", optional($.optional_flag)),
@@ -33,7 +34,7 @@ module.exports = grammar({
         field("field_type", $.identifier)
       ),
     // -------- PERMISSION DECLARATION.
-    permission_declaration: ($) =>
+    permission_declaration: $ =>
       seq(
         "permission",
         field("name", $.identifier),
@@ -41,7 +42,7 @@ module.exports = grammar({
         field("body", $.query)
       ),
     // -------- QUERY DECLARATION.
-    query_declaration: ($) =>
+    query_declaration: $ =>
       seq(
         "query",
         field("name", $.identifier),
@@ -49,10 +50,10 @@ module.exports = grammar({
         field("guards", optional($.guards_list)),
         field("body", $.query)
       ),
-    guards_list: ($) => repeat1($.guard),
-    guard: ($) => seq("allow", field("selection", $.call)),
+    guards_list: $ => repeat1($.guard),
+    guard: $ => seq("allow", field("selection", $.call)),
     // -------- ACTION DECLARATION.
-    action_declaration: ($) =>
+    action_declaration: $ =>
       seq(
         "action",
         field("name", $.identifier),
@@ -62,48 +63,48 @@ module.exports = grammar({
         field("body", optional($.statements)),
         "}"
       ),
-    statements: ($) => repeat1($._statement),
-    _statement: ($) =>
+    statements: $ => repeat1($._statement),
+    _statement: $ =>
       choice($.create_statement, $.delete_statement, $.update_statement),
-    create_statement: ($) =>
+    create_statement: $ =>
       seq(
         "create",
         field("name", $.identifier),
         field("binding", $.binding),
         ";"
       ),
-    delete_statement: ($) =>
+    delete_statement: $ =>
       seq(
         "delete",
         field("target", choice($.query, $.parameter_reference)),
         ";"
       ),
-    update_statement: ($) =>
+    update_statement: $ =>
       seq(
         "update",
         field("target", choice($.query, $.parameter_reference)),
         field("patch", $.binding),
         ";"
       ),
-    binding: ($) =>
+    binding: $ =>
       prec(1, seq("{", field("body", optional($.binding_fields)), "}")),
-    binding_fields: ($) => seq(commaSep1($.binding_field), optional(",")),
-    binding_field: ($) =>
+    binding_fields: $ => seq(commaSep1($.binding_field), optional(",")),
+    binding_field: $ =>
       seq(field("name", $.identifier), ":", field("value", $._binding_value)),
-    _binding_value: ($) => choice($._value, $.binding),
+    _binding_value: $ => choice($._value, $.binding),
     // ------- COMMON
-    identifier: ($) => /[a-zA-Z][a-zA-Z0-9_]*/,
-    parameter_name: ($) => /\$[a-zA-Z][a-zA-Z0-9_]*/,
-    parameters_list: ($) => seq("(", commaSep($.parameter), optional(","), ")"),
-    optional_flag: ($) => token("?"),
-    parameter: ($) =>
+    identifier: $ => /[a-zA-Z][a-zA-Z0-9_]*/,
+    parameter_name: $ => /\$[a-zA-Z][a-zA-Z0-9_]*/,
+    parameters_list: $ => seq("(", commaSep($.parameter), optional(","), ")"),
+    optional_flag: $ => token("?"),
+    parameter: $ =>
       seq(
         field("name", $.parameter_name),
         field("optional", optional($.optional_flag)),
         ":",
         field("type", $.identifier)
       ),
-    _value: ($) =>
+    _value: $ =>
       choice(
         $.query,
         $.call,
@@ -113,25 +114,25 @@ module.exports = grammar({
         $.variable,
         $.uri
       ),
-    variable: ($) =>
+    variable: $ =>
       seq(
         field("base", choice($.parameter_reference, $.type_reference, $.user)),
         field("field", optional($.uri))
       ),
-    uri: ($) => repeat1(seq(".", $.identifier)),
-    _boolean: ($) => choice($.true_literal, $.false_literal),
-    true_literal: ($) => token("true"),
-    false_literal: ($) => token("false"),
-    parameter_reference: ($) => $.parameter_name,
-    type_reference: ($) => $.identifier,
-    user: ($) => token("%user"),
-    query: ($) => seq("{", optional($._query_body), "}"),
-    _query_body: ($) => seq(commaSep1($.call), optional(",")),
-    call: ($) =>
+    uri: $ => repeat1(seq(".", $.identifier)),
+    _boolean: $ => choice($.true_literal, $.false_literal),
+    true_literal: $ => token("true"),
+    false_literal: $ => token("false"),
+    parameter_reference: $ => $.parameter_name,
+    type_reference: $ => $.identifier,
+    user: $ => token("%user"),
+    query: $ => seq("{", optional($._query_body), "}"),
+    _query_body: $ => seq(commaSep1($.call), optional(",")),
+    call: $ =>
       seq(field("callee", $.identifier), field("arguments", $.arguments)),
-    arguments: ($) => seq("(", commaSep(optional($._value)), ")"),
-    number: (_) => /[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/,
-    string: ($) =>
+    arguments: $ => seq("(", commaSep(optional($._value)), ")"),
+    number: _ => /[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/,
+    string: $ =>
       choice(
         seq(
           '"',
@@ -148,7 +149,7 @@ module.exports = grammar({
           "'"
         )
       ),
-    escape_sequence: ($) =>
+    escape_sequence: $ =>
       token.immediate(
         seq(
           "\\",
@@ -162,11 +163,9 @@ module.exports = grammar({
         )
       ),
     // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
-    comment: ($) =>
-      token(
-        choice(seq("//", /.*/), seq("/*", /[^*]*\*+([^/*][^*]*\*+)*/, "/"))
-      ),
-  },
+    comment: $ =>
+      token(choice(seq("//", /.*/), seq("/*", /[^*]*\*+([^/*][^*]*\*+)*/, "/")))
+  }
 });
 
 function commaSep1(rule) {
